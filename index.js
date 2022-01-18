56e5db629f45098c7ac4a96c2f59302c30d4da03
Md.Mithun Hossain <mhosen751@gmail.com>
2022-01-19 00:28:30 +0600

    portfolio & some features addes

--- index.js
+++ index.js
@@ -27,3 +27,26 @@ modalCloseBtns.forEach((modalCloseBtn) =>{
     });
 });
 
+
+//portfolio section model
+const portfolioModals = document.querySelectorAll(".portfolio-model");
+const imgCards = document.querySelectorAll(".img-card");
+const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
+
+var portfolioModal = function(modalClick){
+    portfolioModals[modalClick].classList.add("active");
+}
+
+imgCards.forEach((imgCard, i) => {
+    imgCard.addEventListener("click", () =>{
+        portfolioModal(i);
+    });
+});
+portfolioCloseBtns.forEach((modalCloseBtn) =>{
+    modalCloseBtn.addEventListener("click", () =>{
+        portfolioModals.forEach((portfolioModalView) =>{
+            portfolioModalView.classList.remove("active");
+        });
+    });
+});
+
